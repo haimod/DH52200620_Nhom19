a0d45b0afd5dd9a05d800672fe54816795cc1410
Tran Duc Hai <dh52200620@student.stu.edu.vn>
2025-11-20 19:03:26 +0700

    cap nhat route / de hien thi du lieu bang Thiet Bi tren index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('index'); // trả về view resources/views/index.blade.php
+});
+
+Route::get('/',function(){
+$thietBi = DB::table('thietbi')->get();// Lấy tất cả dữ liệu bảng ThietBi
+return view('thietbi',['thietbi' => $thietBi]);// Truyền dữ liệu vào view
+
+
 });

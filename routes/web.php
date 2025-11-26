ff60d0a55d74e9a5c4cedf12a178e53d312b2eb0
Tran Duc Hai <dh52200620@student.stu.edu.vn>
2025-11-26 10:59:06 +0700

    cap nhap routes / voi HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
-use Illuminate\Support\Facades\DB;
-
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index'); // trả về view resources/views/index.blade.php
-});
-
-Route::get('/',function(){
-$thietBi = DB::table('thietbi')->get();// Lấy tất cả dữ liệu bảng ThietBi
-return view('index',['thietbi' => $thietBi]);// Truyền dữ liệu vào view
-
-
-});
+Route::get('/', [HomeController::class, 'index']);

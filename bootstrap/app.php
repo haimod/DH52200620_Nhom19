475ade04b0bb00232139a64fc4d6da3c36df239c
Tran Duc Hai <dh52200620@student.stu.edu.vn>
2025-12-11 14:25:12 +0700

    Điều chỉnh cấu hình trong bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CheckAdmin; // 1. Import middleware của bạn
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+           // 2. Đăng ký tên gọi tắt là 'admin'
+        $middleware->alias([
+            'admin' => CheckAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
